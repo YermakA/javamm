0a9bce841b8ee672c21e282ee184f9f4de2e7123
YermakA <ermak.080@mail.ru>
2022-03-15 03:13:21 +0800

    has been added package:Exception and have been created classes ConfigException, JavammError

--- code/src/main/java/module-info.java
+++ code/src/main/java/module-info.java
@@ -3,4 +3,7 @@
  * @link https://github.com/YermakA/javamm
  */
 module javamm.code {
+    exports com.example.javamm.code.fragment;
+    exports com.example.javamm.code.fragment.operation;
+    exports com.example.javamm.code.exception;
 }
