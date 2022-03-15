291aaacb0a493d6c9ec211f0202d80931258cc6e
YermakA <ermak.080@mail.ru>
2022-03-15 22:55:57 +0800

    compiler model was created and its components were implemented

---
+++ compiler/src/main/java/com/example/javamm/compiler/JavammSyntaxError.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022.  https://github.com/YermakA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.javamm.compiler;
+
+import com.example.javamm.code.exception.JavammError;
+
+/**
+ * @author YermakA
+ * @link https://github.com/YermakA/javamm
+ */
+public class JavammSyntaxError extends JavammError {
+
+
+    protected JavammSyntaxError(final String message) {
+        super(message);
+    }
+}
--- compiler/src/main/java/module-info.java
+++ compiler/src/main/java/module-info.java
@@ -4,4 +4,8 @@
  */
 module javamm.compiler {
     requires javamm.code;
+
+    exports com.example.javamm.compiler.;
+    exports com.example.javamm.compiler.model;
+    exports com.example.javamm.compiler.component;
 }
